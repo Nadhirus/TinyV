2829c144897ca1947807172341becb4d8c17f1b5
BZN Nadhir <bouzianenadirus@gmail.com>
2024-12-22 00:06:25 +0100

    - Implemented a 3x1 configurable mux for datapath design

---
+++ src/core/mux3x1.sv
@@ -0,0 +1,34 @@
+`default_nettype none
+`include "types.sv"
+
+module mux3x1 #(
+  parameter DATA_WIDTH = `DATA_WIDTH ;
+)(
+  input logic [DATA_WIDTH-1 : 0] A,
+  input logic [DATA_WIDTH-1 : 0] B,
+  input logic [DATA_WIDTH-1 : 0] C,
+  input logic [1:0] sel, 
+  output logic [DATA_WIDTH-1 : 0] result
+);
+
+  always_comb begin
+    case (sel)
+      2'b00: result = A;
+      2'b01: result = B;
+      2'b10: result = C;
+      default: result = DATA_WIDTH'(0);
+    endcase
+  end
+
+endmodule
+
+
+
+
+
+
+
+
+
+
+
